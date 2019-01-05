55438ef018c5517534cce8662eb10bd0f3983103
Agus Safarudin <42315161+agussafarudin@users.noreply.github.com>
2019-01-05 23:43:18 +0700

    safarudin

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+    char h{5};
+    for (i=0;i<5;i++)
+    {
+        printf("Elemen ke-%1 = %d\n",i,h{1});
+    }
+    getch();
+    return 0;
+}
